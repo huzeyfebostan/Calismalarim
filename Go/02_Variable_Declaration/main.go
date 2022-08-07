2b9c28b93f5a73125d1d853df467c6f9558ee636
huzeyfe99 <huzeyfebostan99@gmail.com>
2022-08-07 20:06:10 +0300

    go_update

---
+++ Go/02_Variable_Declaration/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Hoze"
+	var age int = 22
+	var isStudent bool = true
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isStudent)
+}
