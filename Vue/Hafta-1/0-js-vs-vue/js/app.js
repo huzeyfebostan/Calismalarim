e0a202dc3b4572616bfc54783797ee7d98ab52fc
huzeyfebostan <huzeyfebostan99@gmail.com>
2023-07-12 16:30:15 +0300

    fix

--- Vue/Hafta-1/0-js-vs-vue/js/app.js
+++
@@ -1,9 +0,0 @@
-const todoText = document.querySelector('#todoText');
-const addBtn = document.querySelector('#addBtn');
-const todoList = document.querySelector('#todoList');
-
-addBtn.addEventListener("click", () => {
-  const listItem = document.createElement('li');
-  listItem.textContent = todoText.value;
-  todoList.append(listItem);
-});
